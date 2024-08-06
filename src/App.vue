99edf3c0f28bfd31903525926efbc4b00340a032
OUMARNDIAYE49 <oumar.ndiaye4952@gmail.com>
2024-08-06 14:07:02 +0000

    first commit

---
+++ src/App.vue
@@ -0,0 +1,67 @@
+
+
+<template>
+<div>
+    <HelloWord>
+
+        <!-- <template v-slot:name>
+            {{user.name }}
+        </template>
+        
+        <template v-slot:age>
+            {{user.age }}
+        </template>
+
+        <p>{{user.name}}</p> -->
+
+        <div
+            class="text.green"
+        >
+        {{ user.name }}
+        </div>
+    </HelloWord>
+
+
+
+    <!-- <div v-if="user.age < 15"
+    >
+        
+          mon age
+    </div> 
+    
+    <div>
+        else
+    </div> -->
+    <!-- <input type="text" v-model="user.name" v-on:input="display "  >
+    <input type="text" v-model="user.age"   > -->
+</div>
+</template>
+
+<script setup>
+import HelloWord from './components/HelloWord.vue';
+
+import {ref} from 'vue';
+
+const user = ref({
+    name: 'Ndiaye',
+    age: 90,
+});
+
+
+const display = () => {
+    // console.log(name, age );
+    
+}
+display()
+</script>  
+
+<style scoped>
+.text.red{
+    color: indianred ;  
+} 
+.text.green{
+    color: green;
+}
+</style> 
+
+  
---
+++ src/components/HelloWord.vue
@@ -0,0 +1,20 @@
+<template>
+	<div>
+	  <h1>Hello World</h1>
+	  <!-- <p>{{ props.user.name }} {{ props.user.age }}</p> -->
+
+	<slot name="name">
+	
+	</slot>
+
+	<slot name="age">
+	
+	 </slot>
+	</div>
+  </template>
+  
+  <script setup>
+
+
+   const props = defineProps(['user']);
+  </script>   
